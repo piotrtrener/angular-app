1d8b9bb8270892a5b53735785a7a9f2e4b11ba70
Piotr Kania <pkania@web-coders.pl>
2022-05-28 14:08:48 +0200

    contacts module added

---
+++ src/app/data/contacts-data.ts
@@ -0,0 +1,6 @@
+export const CONTACTS = [
+
+    {id: 1, surname: 'Kowalski', firstName: 'Jan', phoneNumber: 11111111},
+    {id: 2, surname: 'Nowak', firstName: 'Anna', phoneNumber: 2222222}
+
+];
---
+++ src/app/models/contact-model.ts
@@ -0,0 +1,8 @@
+export interface ContactModel {
+
+    id: number;
+    surname: string;
+    firstName: string;
+    phoneNumber: number;
+
+}
